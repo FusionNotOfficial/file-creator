27c2a8dd255712d012ddec66f10868fa67971409
Mikita Shabanau <bags4circles@gmail.com>
2023-07-13 20:57:48 +0900

    add FileCreator.sln

---
+++ FileCreator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileCreator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = string.Empty;
+            string path = string.Empty;
+            Console.WriteLine("Enter file name: ");
+            fileName = Console.ReadLine();
+            path = "F:\\" + fileName + ".txt";
+            Console.WriteLine("Enter some text here: ");
+            string text = Console.ReadLine();
+            File.WriteAllText(path, text);
+        }
+    }
+}
